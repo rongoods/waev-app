8ce7f36b7fa719716edc05808b6b439aa90d237f
Ron McCoy <rongoods@gmail.com>
2023-12-07 08:30:38 +0100

    alter post api index

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -10,8 +10,8 @@ export default async function handler(request, response) {
   } else if (request.method === "POST") {
     try {
       const postData = request.body;
-      console.log("postData: ", postData);
       await Post.create(postData);
+      console.log(postData);
       return response.status(201).json({ status: "Post created" });
     } catch (error) {
       response.status(400).json({ error: error.message });
@@ -21,33 +21,8 @@ export default async function handler(request, response) {
   try {
     const posts = await Post.find();
 
-    response.status(200).json(posts);
+    res.status(200).json(posts);
   } catch (error) {
     console.log(error);
   }
 }
-
-//  below is all tourio test
-
-//COMPLETE!!!!!!
-
-// import dbConnect from "../../../db/connect";
-// import Post from "../../../db/models/Post";
-
-// export default async function handler(request, response) {
-//   await dbConnect();
-
-//   if (request.method === "GET") {
-//     const posts = await Post.find();
-//     return response.status(200).json(posts);
-//   } else if (request.method === "POST") {
-//     try {
-//       const postData = request.content;
-//       console.log("api data: ", postData);
-//       await Post.create(postData);
-//       return response.status(201).json({ status: "Post created" });
-//     } catch (err) {
-//       response.status(400).json({ error: err.message });
-//     }
-//   }
-// }
